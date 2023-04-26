2d6519bb24a4a94de349aa5e702b1e63580a65de
nandan-varma <nandanvarma.me@gmail.com>
2023-04-26 19:26:10 +0530

    implemented playing with friends

---
+++ pages/signup.jsx
@@ -0,0 +1,53 @@
+import { useState } from 'react';
+import firebase from '../lib/firebase'
+import 'firebase/auth';
+import { useRouter } from 'next/router';
+
+export default function Signup() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+    const router = useRouter()
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // handle form submission using firebase authentication
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then((userCredential) => {
+        // Signed in
+        var user = userCredential.user;
+        router.push('/');
+        // ...
+      })
+      .catch((error) => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // ...
+      });
+  };
+
+  return (
+    <div>
+      <h1>Signup</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <br />
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <br />
+        <button type="submit">Signup</button>
+      </form>
+    </div>
+  );
+}
